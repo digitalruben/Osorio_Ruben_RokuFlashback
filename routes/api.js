e4db4a1c0f6e1c97f65b9b4b1df3c2b2f6136822
Ruben Osorio <rubendaosorio@gmail.com>
2021-04-12 20:19:55 -0400

    updating server api

--- routes/api.js
+++ routes/api.js
@@ -1,21 +1,21 @@
 const express = require('express');
-
-
+// express router handles incoming requests and directs them where they need to go
+// like a traffc cop
 const router = express.Router();
 
+// think of route handlers like PHP functions
 
 router.get("/", (req, res) => {
-
-
+    // res.json = echo json_encode(...) in PHP
     res.json({message: "you hit the api route"});
 });
 
+// this is the /api/users route handler
 router.get("/users", (req, res) => {
-
-
-
+    // run a SQL query here
+    // res.json(query result here)
+    // echo a message -> just like a PHP
     res.json({message: "all users route"});
 })
-
 
 module.exports = router;
