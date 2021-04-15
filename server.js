c097ff567442a6fa17beaec452464201789e1869
Ruben Osorio <rubendaosorio@gmail.com>
2021-04-15 03:49:56 -0400

    adding server routing and DB users

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 5050;
+
+const port = process.env.PORT || 5000;
 
 // this catches every route request - every time you change your location bar, this function reacts to it and intercepts the route request
 app.use((req, res, next) => {
     console.log('incoming request');
-    console.log(port);
+    //console.log(port);
 
     // next is the original route request ie. /api/users
     next(); // => send the user to the route the requested
@@ -14,6 +15,7 @@ app.use((req, res, next) => {
 
 // use the API route file to handle API routes (/api/users, /api/:user/:id, ect)
 app.use("/api", require("./routes/api"));
+app.use("/ums", require("./routes/ums"));
 
 // run the app at the PORT
 app.listen(port, () => {
