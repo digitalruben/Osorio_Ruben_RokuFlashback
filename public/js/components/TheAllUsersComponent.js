866ceb55f0ae13b6187fef30c53374cdeb9df529
Ruben Osorio <rubendaosorio@gmail.com>
2021-04-16 17:02:30 -0400

    updating files

--- public/js/components/TheAllUsersComponent.js
+++ public/js/components/TheAllUsersComponent.js
@@ -1,17 +1,35 @@
+import UserComponent from './TheUserComponent.js';
+
 export default {
     name: "TheAllUsersComponent",
-    
-    template: `
-    <section class="container">
-        <div class="row">
-            <div class="col-sm-12">
-                <h1>This is the all users component!!</h1>
-            </div>
-        </div>
-    </section>
-    `,
 
-    created: function() {
-        console.log('all users component is ready');
-    }
+	template: `
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12">
+				<h1 class="user-message">{{ message }}</h1>
+			</div>
+
+			<user v-for="(user, index) in userList" :liveuser="user" :key="index"></user>
+		</div>
+	</div>
+	`,
+
+	created: function() {
+        // get all users fron DB
+        fetch(`/ums/admin/getusers`)
+            .then(res => res.json())
+            .then(data => this.userList = data)
+        .catch((err) => console.error(err));
+	},
+
+	data() {
+	  return (
+          {	message: `Who's Using Roku?`, userList: [] }
+      )
+	},
+
+	components: {
+		user: UserComponent
+	}
 }
--- public/js/main.js
+++ public/js/main.js
@@ -1,36 +1,54 @@
 import LoginComponent from './components/TheLoginComponent.js';
 import AllUsers from './components/TheAllUsersComponent.js';
+import HomeComponent from './components/TheHomeComponent.js';
 
 const router = new VueRouter({
     routes: [
-        { path: '/', name: 'root', component: LoginComponent },
-        { path: '/users', name: 'users', component: AllUsers }
+        { path: '/', name: 'root', component: LoginComponent, beforeEnter: (to, from, next) => {
+            // if you're authenticated (set in localstorage), then go to the home page
+            if (localStorage.getItem('cacheduser')) {
+                let user = JSON.parse(localStorage.getItem('cacheduser'));
+                next({name: 'home', params: {currentUser: user}});
+            } else {
+                next();
+            }
+        }},
+        { path: '/users', name: 'users', component: AllUsers },
+        { path: '/home', name: 'home', component: HomeComponent, props: true }
     ]
 });
 
 (() =>  {
     const vm = new Vue({
         data: {
-            allMovies: []
+            authenticated: false,
+            isAdmin: false,
+            currentUser: undefined
         },
 
         created: function() {
-            //fetch('/api/movies')
-            //    .then(res =>res.json())
-            //    .then(data => {
-            //        // show the data in table form
-            //        console.table(data);
-            //        this.allMovies = data;
-            // })
-            // .catch(err => console.error(error));
+            // use this if you want 
+
+
+
         },
 
         methods: {
+            logout() {
+                // remove the cached user from localstorage, if it exists
+                if (localStorage.getItem('cacheduser')) {
+                    localStorage.removeItem('cacheduser');
+                }
 
-        },
+                this.$router.push({ name: "root" });
+                this.currentUser = undefined;
+            },
 
-        components: {
-            // moviethumb: TheMovieThumb
+            authenticateuser(user) {
+                // debugger;
+                this.currentUser = user;
+            }
+
         },
 
         router
